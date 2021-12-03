fc271e66e4de675122f351d0e00afd4be57345a9
lmwamba-hai <bmwamba@stedwards.edu>
2021-12-02 19:30:59 -0600

    Initial commit

---
+++ src/main/java/com/company/netflixcapstone/NetflixCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.company.netflixcapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixCapstoneApplication.class, args);
+	}
+
+}
