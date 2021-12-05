d4e39f1bc80fedf608d2bd88da10f8fc5146d556
Nicolas Dalton <ndalton1@stedwards.edu>
2021-12-05 17:36:14 -0600

    Setup ConsoleController APIs

---
+++ src/main/java/com/company/netflixcapstone/controllers/ConsoleController.java
@@ -0,0 +1,46 @@
+package com.company.netflixcapstone.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ConsoleController {
+
+    @RequestMapping(value= "/consoles", method= RequestMethod.POST)
+    @ResponseStatus(value= HttpStatus.CREATED)
+    public Console createConsole(@RequestBody Console console) {
+        return null;
+    }
+
+    @RequestMapping(value= "/consoles/{id}", method= RequestMethod.GET)
+    @ResponseStatus(value= HttpStatus.OK)
+    public Console getConsole(@PathVariable int id) {
+        return null;
+    }
+
+    @RequestMapping(value= "/consoles", method= RequestMethod.GET)
+    @ResponseStatus(value= HttpStatus.OK)
+    public List<Console> getAllConsoles() {
+        return null;
+    }
+
+    @RequestMapping(value= "/consoles", method= RequestMethod.PUT)
+    @ResponseStatus(value= HttpStatus.NO_CONTENT)
+    public void updateConsole(@RequestBody Console console) {
+
+    }
+
+    @RequestMapping(value= "/consoles", method= RequestMethod.DELETE)
+    @ResponseStatus(value= HttpStatus.NO_CONTENT)
+    public void deleteConsole(@PathVariable int id) {
+
+    }
+
+    @RequestMapping(value= "/consoles/{manufacturer}", method= RequestMethod.GET)
+    @ResponseStatus(value= HttpStatus.OK)
+    public List<Console> getConsolesByManufacturer(@PathVariable String manufacturer) {
+        return null;
+    }
+}
