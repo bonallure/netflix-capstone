7a0897906dfa4de5d6a28b1817f30212afee673a
Nicolas Dalton <ndalton1@stedwards.edu>
2021-12-05 16:38:02 -0600

    Setup GameController APIs

---
+++ src/main/java/com/company/netflixcapstone/controllers/GameController.java
@@ -0,0 +1,59 @@
+package com.company.netflixcapstone.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class GameController {
+
+    @RequestMapping(value= "/games", method= RequestMethod.POST)
+    @ResponseStatus(value= HttpStatus.CREATED)
+    public Game createGame(@RequestBody Game game) {
+        return null;
+    }
+
+    @RequestMapping(value= "/games/{id}", method= RequestMethod.GET)
+    @ResponseStatus(value= HttpStatus.OK)
+    public Game getGame(@PathVariable int id) {
+        return null;
+    }
+
+    @RequestMapping(value= "/games", method= RequestMethod.GET)
+    @ResponseStatus(value= HttpStatus.OK)
+    public List<Game> getAllGames() {
+        return null;
+    }
+
+    @RequestMapping(value= "/games", method= RequestMethod.PUT)
+    @ResponseStatus(value= HttpStatus.NO_CONTENT)
+    public void updateGame(@RequestBody Game game) {
+
+    }
+
+    @RequestMapping(value= "/games/{id}", method= RequestMethod.DELETE)
+    @ResponseStatus(value= HttpStatus.NO_CONTENT)
+    public void deleteGame(@PathVariable int id) {
+
+    }
+
+    @RequestMapping(value= "/games/{studio}", method= RequestMethod.GET)
+    @ResponseStatus(value= HttpStatus.OK)
+    public List<Game>  getGamesByStudio(@PathVariable String studio) {
+        return null;
+    }
+
+    @RequestMapping(value= "/games/{esrb}", method= RequestMethod.GET)
+    @ResponseStatus(value= HttpStatus.OK)
+    public List<Game> getGamesByESRB(@PathVariable String esrb) {
+        return null;
+    }
+
+    @RequestMapping(value= "/value/{title}", method= RequestMethod.GET)
+    @ResponseStatus(value= HttpStatus.OK)
+    public List<Game> getGamesByTitle(@PathVariable String title) {
+        return null;
+    }
+
+}
