1d3444fd87e09db668553c3bc969ba129d5f285c
lmwamba-hai <bmwamba@stedwards.edu>
2021-12-09 18:49:54 -0600

    service layer initial commit

---
+++ src/test/java/com/company/netflixcapstone/dao/impl/ConsoleDAOImplTest.java
@@ -0,0 +1,15 @@
+package com.company.netflixcapstone.dao.impl;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by bonallure on 12/9/21
+ */
+public class ConsoleDAOImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
