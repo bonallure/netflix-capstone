862c7b882b6318aaaf7b00747ed272e2c9435ce7
lmwamba-hai <bmwamba@stedwards.edu>
2021-12-10 19:05:41 -0600

    autoriwired the consoledao to the ConsoleDAOImplTest

--- src/test/java/com/company/netflixcapstone/dao/impl/ConsoleDAOImplTest.java
+++ src/test/java/com/company/netflixcapstone/dao/impl/ConsoleDAOImplTest.java
@@ -1,15 +1,26 @@
 package com.company.netflixcapstone.dao.impl;
 
+import com.company.netflixcapstone.dao.ConsoleDAO;
 import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by bonallure on 12/9/21
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class ConsoleDAOImplTest {
+
+    @Autowired
+    private ConsoleDAO consoleDAO;
 
     @Before
     public void setUp() throws Exception {
     }
+
 }
