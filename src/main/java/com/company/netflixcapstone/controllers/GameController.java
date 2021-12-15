85e5bcd93def09df578ba748d29ab0d800639b33
Nicolas Dalton <ndalton1@stedwards.edu>
2021-12-15 14:25:48 -0600

    implement CRUD methods for GameController

--- src/main/java/com/company/netflixcapstone/controllers/GameController.java
+++ src/main/java/com/company/netflixcapstone/controllers/GameController.java
@@ -1,6 +1,8 @@
 package com.company.netflixcapstone.controllers;
 
 import com.company.netflixcapstone.model.Game;
+import com.company.netflixcapstone.serviceLayer.GameStoreServiceLayer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,35 +10,37 @@ import java.util.List;
 
 @RestController
 public class GameController {
+    @Autowired
+    private GameStoreServiceLayer serviceLayer;
 
     @RequestMapping(value= "/games", method= RequestMethod.POST)
     @ResponseStatus(value= HttpStatus.CREATED)
     public Game createGame(@RequestBody Game game) {
-        return null;
+        return serviceLayer.addGame(game);
     }
 
     @RequestMapping(value= "/games/{id}", method= RequestMethod.GET)
     @ResponseStatus(value= HttpStatus.OK)
     public Game getGame(@PathVariable int id) {
-        return null;
+        return serviceLayer.getGame(id);
     }
 
     @RequestMapping(value= "/games", method= RequestMethod.GET)
     @ResponseStatus(value= HttpStatus.OK)
     public List<Game> getAllGames() {
-        return null;
+        return serviceLayer.getAllGames();
     }
 
     @RequestMapping(value= "/games", method= RequestMethod.PUT)
     @ResponseStatus(value= HttpStatus.NO_CONTENT)
     public void updateGame(@RequestBody Game game) {
-
+        serviceLayer.updateGame(game);
     }
 
     @RequestMapping(value= "/games/{id}", method= RequestMethod.DELETE)
     @ResponseStatus(value= HttpStatus.NO_CONTENT)
     public void deleteGame(@PathVariable int id) {
-
+        serviceLayer.deleteGame(id);
     }
 
     @RequestMapping(value= "/games/studio/{studio}", method= RequestMethod.GET)
