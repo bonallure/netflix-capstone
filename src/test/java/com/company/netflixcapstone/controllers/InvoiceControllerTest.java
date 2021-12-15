7ec40b62ef8b04e76c1a399300cbb82c7419e2cb
Nicolas Dalton <ndalton1@stedwards.edu>
2021-12-15 15:11:48 -0600

    fixed test case for invoice controller

--- src/test/java/com/company/netflixcapstone/controllers/InvoiceControllerTest.java
+++ src/test/java/com/company/netflixcapstone/controllers/InvoiceControllerTest.java
@@ -5,9 +5,12 @@ import com.company.netflixcapstone.serviceLayer.GameStoreServiceLayer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -16,7 +19,8 @@ import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+@RunWith(SpringRunner.class)
+@WebMvcTest(InvoiceController.class)
 public class InvoiceControllerTest {
 
     //wiring to MockMvc object
@@ -37,20 +41,22 @@ public class InvoiceControllerTest {
     @Test
     public void createInvoice() throws Exception {
         Invoice invoice = new Invoice();
-        invoice.setSubtotal(new BigDecimal("80.00"));
-        invoice.setTotal(new BigDecimal("70.00"));
-        invoice.setProcessingFee(new BigDecimal("1.98"));
-        invoice.setTax(new BigDecimal(".04"));
-        invoice.setUnitPrice(new BigDecimal("14.99"));
-        invoice.setItemType("tshirt");
-        invoice.setItemId(1);
-        invoice.setQuantity(1);
+        // User Info
         invoice.setName("Bob Johnson");
         invoice.setStreet("6th Street");
         invoice.setCity("Austin");
         invoice.setState("TX");
         invoice.setZipcode("78728");
-
+        // Item Info
+        invoice.setItemType("tshirt");
+        invoice.setItemId(1);
+        invoice.setUnitPrice(new BigDecimal("14.99"));
+        //Order Info
+        invoice.setQuantity(1);
+        invoice.setSubtotal(new BigDecimal("80.00"));
+        invoice.setTax(new BigDecimal(".04"));
+        invoice.setProcessingFee(new BigDecimal("1.98"));
+        invoice.setTotal(new BigDecimal("70.00"));
 
         String inputJson = mapper.writeValueAsString(invoice);
 
