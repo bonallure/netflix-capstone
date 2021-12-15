6b6e15b62c69200be6366f2644d1da1a9e14e32b
Nicolas Dalton <ndalton1@stedwards.edu>
2021-12-15 14:43:29 -0600

    implement ConsoleController

--- src/main/java/com/company/netflixcapstone/controllers/ConsoleController.java
+++ src/main/java/com/company/netflixcapstone/controllers/ConsoleController.java
@@ -1,6 +1,8 @@
 package com.company.netflixcapstone.controllers;
 
 import com.company.netflixcapstone.model.Console;
+import com.company.netflixcapstone.serviceLayer.GameStoreServiceLayer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,34 +11,37 @@ import java.util.List;
 @RestController
 public class ConsoleController {
 
+    @Autowired
+    private GameStoreServiceLayer serviceLayer;
+
     @RequestMapping(value= "/consoles", method= RequestMethod.POST)
     @ResponseStatus(value= HttpStatus.CREATED)
     public Console createConsole(@RequestBody Console console) {
-        return null;
+        return serviceLayer.addConsole(console);
     }
 
     @RequestMapping(value= "/consoles/{id}", method= RequestMethod.GET)
     @ResponseStatus(value= HttpStatus.OK)
     public Console getConsole(@PathVariable int id) {
-        return null;
+        return serviceLayer.getConsole(id);
     }
 
     @RequestMapping(value= "/consoles", method= RequestMethod.GET)
     @ResponseStatus(value= HttpStatus.OK)
     public List<Console> getAllConsoles() {
-        return null;
+        return serviceLayer.getAllConsoles();
     }
 
     @RequestMapping(value= "/consoles", method= RequestMethod.PUT)
     @ResponseStatus(value= HttpStatus.NO_CONTENT)
     public void updateConsole(@RequestBody Console console) {
-
+        serviceLayer.updateConsole(console);
     }
 
     @RequestMapping(value= "/consoles/{id}", method= RequestMethod.DELETE)
     @ResponseStatus(value= HttpStatus.NO_CONTENT)
     public void deleteConsole(@PathVariable int id) {
-
+        serviceLayer.deleteConsole(id);
     }
 
     @RequestMapping(value= "/consoles/manufacturer/{manufacturer}", method= RequestMethod.GET)
